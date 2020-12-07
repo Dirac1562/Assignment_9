3a7beebe12d48ef922936d723bf4e8d8d77a2386
Dirac Murairi <diracm8@gmail.com>
2020-12-07 12:33:01 +0200

    submit assignment

---
+++ books.py
@@ -0,0 +1,13 @@
+class Book:
+    """
+    -----
+    Book
+
+    This is the class that instantiates the books
+
+    """
+    def __init__(self, name, id, status, author):
+        self.name = name
+        self.id = id
+        self.status = status
+        self.author = author
